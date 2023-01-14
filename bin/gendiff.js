501c70f57b04ddd7259b7e356748f40632d288b4
Aleksandr Gushchin <asfiks88@gmail.com>
2023-01-14 16:34:36 +0300

    add files, fist and second step

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+program
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.0')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format <type>', 'output format')
+    .parse();
