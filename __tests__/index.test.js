699e8eec3fb6c449668f8dfee8d612d7a820334a
asfiks <asfiks88@gmail.com>
2023-01-19 19:25:51 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,18 @@
 import genDiff from '../src/index.js';
 import path from 'path';
-//import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-//const getPath = (namefile) => path.join(path.resolve(), '__fixtures__', namefile);
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('genDiff null', () => {
-  expect(genDiff(getPath('file1.json'), getPath('file2.json'))).toEqual('There is no data in JSON');
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual('There is no data in JSON');
 });
 
 test('genDiff', () => {
-  expect(genDiff(getPath('file3.json'), getPath('file4.json'))).toEqual('- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true');
+  expect(genDiff(getFixturePath('file3.json'), getFixturePath('file4.json'))).toEqual('- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true');
 });
