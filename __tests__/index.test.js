fd7b479002125d1131ecc40d2633e111778ffa16
asfiks <asfiks88@gmail.com>
2023-02-05 14:03:38 +0300

    update step 6, problem with test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,34 +1,23 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
-
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 
+test('diff two json with stylish', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))
+      .toEqual(fs.readFileSync(getFixturePath('expectedStylish.txt'), 'utf8'));
+});
 
-
-test('genDiff json files no data', () => {
+/*test('genDiff json files no data', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual('There is no data in files');
 });
 
-test('genDiff json files', () => {
-  expect(genDiff(getFixturePath('file3.json'), getFixturePath('file4.json'))).toEqual('- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true');
-});
-
-test('genDiff if one of file json no data', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file4.json'))).toEqual('+ host: hexlet.io\n+ timeout: 20\n+ verbose: true');
-});
 
 test('genDiff yml files no data', () => {
   expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'))).toEqual('There is no data in files');
 });
-
-test('genDiff', () => {
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual('- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true');
-});
-
-test('genDiff if one of file yml no data', () => {
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file3.yml'))).toEqual('- follow: false\n- host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50');
-});
+*/
--- src/parsers.js
+++ src/parsers.js
@@ -9,7 +9,7 @@ const parsersFile = (filePath) => {
     let parse;
     const nameFileArr = filePath.split('.');
     const extension = nameFileArr[nameFileArr.length - 1];
-    if (extension === 'json') parse = JSON.parse(fs.readFileSync(absPath(filePath)));
+    if (extension === 'json') parse = JSON.parse(fs.readFileSync(absPath(filePath), 'utf-8'));
     else if (extension === 'yml' || extension === 'yaml') parse = yaml.load(fs.readFileSync(absPath(filePath)));
         if (parse === null || parse === undefined) parse = {};
     return parse;
