d869fad86f33c9e469b5dd47cb748a6b306bad5f
asfiks <asfiks88@gmail.com>
2023-02-10 18:42:13 +0300

    fix function genDiff

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import parsersFile from './parsers.js';
 import getDiffTree from './difftree.js';
 import formatter from './formatters/index.js';
 
-const genDiff = (filePath1, filePath2, format) => {
+const genDiff = (filePath1, filePath2, format = 'stylish') => {
   const obj1 = parsersFile(filePath1);
   const obj2 = parsersFile(filePath2);
   const diffTree = getDiffTree(obj1, obj2);
